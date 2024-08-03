cca0777c30f82002e33a399450561687ed1f6a3d
LAAC Admin <laacadmin@MacBook-Air-5.local>
2024-08-03 10:01:37 +0200

    incl. code

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -24,7 +24,8 @@ import html
 import os
 import re
 import requests
-
+from slugify import slugify
+import numpy as np
 
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
@@ -61,6 +62,31 @@ def html_escape(text):
         text = text.replace(pattern, html_escape_table[pattern])
     return text
 
+tags_color = {}
+
+hex_codes = [
+    "#CD5C5C", "#F08080", "#FA8072", "#E9967A", "#FFA07A", "#DC143C", "#FF0000",
+    "#B22222", "#8B0000", "#FFC0CB", "#FFB6C1", "#FF69B4", "#FF1493", "#C71585",
+    "#DB7093", "#FFA07A", "#FF7F50", "#FF6347", "#FF4500", "#FF8C00", "#FFA500",
+    "#FFD700", "#FFFF00", "#FFFFE0", "#FFFACD", "#FAFAD2", "#FFEFD5", "#FFE4B5",
+    "#FFDAB9", "#EEE8AA", "#F0E68C", "#BDB76B", "#E6E6FA", "#D8BFD8", "#DDA0DD",
+    "#EE82EE", "#DA70D6", "#FF00FF", "#FF00FF", "#BA55D3", "#9370DB", "#663399",
+    "#8A2BE2", "#9400D3", "#9932CC", "#8B008B", "#800080", "#4B0082", "#6A5ACD",
+    "#483D8B", "#7B68EE", "#ADFF2F", "#7FFF00", "#7CFC00", "#00FF00", "#32CD32",
+    "#98FB98", "#90EE90", "#00FA9A", "#00FF7F", "#3CB371", "#2E8B57", "#228B22",
+    "#008000", "#006400", "#9ACD32", "#6B8E23", "#808000", "#556B2F", "#66CDAA",
+    "#8FBC8B", "#20B2AA", "#008B8B", "#008080", "#00FFFF", "#00FFFF", "#E0FFFF",
+    "#AFEEEE", "#7FFFD4", "#40E0D0", "#48D1CC", "#00CED1", "#5F9EA0", "#4682B4",
+    "#B0C4DE", "#B0E0E6", "#ADD8E6", "#87CEEB", "#87CEFA", "#00BFFF", "#1E90FF",
+    "#6495ED", "#7B68EE", "#4169E1", "#0000FF", "#0000CD", "#00008B", "#000080",
+    "#191970", "#FFF8DC", "#FFEBCD", "#FFE4C4", "#FFDEAD", "#F5DEB3", "#DEB887",
+    "#D2B48C", "#BC8F8F", "#F4A460", "#DAA520", "#B8860B", "#CD853F", "#D2691E",
+    "#8B4513", "#A0522D", "#A52A2A", "#800000", "#FFFFFF", "#FFFAFA", "#F0FFF0",
+    "#F5FFFA", "#F0FFFF", "#F0F8FF", "#F8F8FF", "#F5F5F5", "#FFF5EE", "#F5F5DC",
+    "#FDF5E6", "#FFFAF0", "#FFFFF0", "#FAEBD7", "#FAF0E6", "#FFF0F5", "#FFE4E1",
+    "#DCDCDC", "#D3D3D3", "#C0C0C0", "#A9A9A9", "#808080", "#696969", "#778899",
+    "#708090", "#2F4F4F", "#000000"
+]
 
 for pubsource in publist:
     parser = bibtex.Parser()
@@ -133,7 +159,6 @@ for pubsource in publist:
             #add venue logic depending on citation type
             venue = venuepretext+b[venuekey].replace("{", "").replace("}","").replace("\\","")
 
-
             citation = citation + " " + html_escape(venue)
             citation = citation + ", " + pub_year + "."
 
@@ -161,6 +186,24 @@ for pubsource in publist:
             #t = publist[pubsource]["type"] if "type" in publist[pubsource] else None
             keywords = bibdata.entries[bib_id].fields["keywords"] if "keywords" in bibdata.entries[bib_id].fields else None
             print(keywords)
+
+            tags = []
+            if "tags" in bibdata.entries[bib_id].fields:
+                tags = [tag.strip() for tag in bibdata.entries[bib_id].fields["tags"].split(",")]
+                print(tags)
+
+            if len(tags):
+                md += "\ntags:"
+                for tag in tags:
+                    tag_id = slugify(tag)
+                    md += f"\n    - tag: {tag}"
+                    md += f"\n      id: {tag_id}"
+
+                    if tag not in tags_color:
+                        tags_color[tag] = np.random.choice(hex_codes)
+                        hex_codes.remove(tags_color[tag])
+
+                    md += f"\n      color: '{tags_color[tag]}'"
 
             if "note" in b and "under review" in  b["note"]:
                 t = "under-review"
